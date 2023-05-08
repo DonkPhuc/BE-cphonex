65f9f4479b7c87aa5f8ceb840c5594aee1512e08
phuc.do <phuc.do@gss-sol.com>
2023-05-08 17:10:42 +0700

    update/add cart pending

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,4 +1,4 @@
-const { Products } = require("../model/model");
+const { Products, Customers } = require("../model/model");
 
 const productsController = {
   addProduct: async (req, res) => {
@@ -6,6 +6,20 @@ const productsController = {
       const newProduct = new Products(req.body);
       const savedNewProduct = await newProduct.save();
       res.status(200).json(savedNewProduct);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  addProductToCart: async (req, res) => {
+    try {
+      const product = await Products.findById(req.params.id);
+      const customer = await Customers.find({ username: req.params.username });
+      const newProduct = new Products(product);
+      const savedNewProduct = await newProduct.save();
+      console.log(savedNewProduct);
+      // await customer.updateOne({ $push: { cart: savedNewProduct._id } });
+      res.status(200).json("product");
     } catch (err) {
       res.status(500).json(err);
     }
@@ -29,26 +43,26 @@ const productsController = {
     }
   },
 
-  //   updateCustomer: async (req, res) => {
-  //     try {
-  //       const customers = await Customers.find({ username: req.params.username });
-  //       console.log(customers[0].password);
-  //       await customers.updateOne({ $set: req.body });
-  //       res.status(200).json("Updated successfully!", customers);
-  //     } catch (err) {
-  //       res.status(500).json(err);
-  //     }
-  //   },
+  updateProduct: async (req, res) => {
+    try {
+      const product = await Products.find({ _id: req.params.id });
+      console.log(product);
+      // await product.updateOne({ $set: req.body });
+      res.status(200).json("Updated successfully!");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 
-  //   deleteCustomer: async (req, res) => {
-  //     try {
-  //       // await Book.updateMany({ author: req.params.id }, { author: null });
-  //       await Customers.findByIdAndDelete(req.params.id);
-  //       res.status(200).json("Deleted successfully!");
-  //     } catch (err) {
-  //       res.status(500).json(err);
-  //     }
-  //   },
+  deleteProduct: async (req, res) => {
+    try {
+      await Customers.updateMany({ cart: req.params.id }, { cart: null });
+      await Products.findByIdAndDelete(req.params.id);
+      res.status(200).json("Deleted successfully!");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 };
 
 module.exports = productsController;
--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,10 @@ const productsController = require("../controllers/productsController");
 const router = require("express").Router();
 
 router.post("/product", productsController.addProduct);
+router.post("/product/update/:id", productsController.updateProduct);
 
+router.get("/product/delete/:id", productsController.deleteProduct);
+router.get("/product/cart/:username/:id", productsController.addProductToCart);
 router.get("/product", productsController.getAllProducts);
 router.get("/product/:id", productsController.getProduct);
 
